0c3b244f5a3c86374c59b50b2b5bfd5ff51666d0
Henry Crofts <hwcrofts@gmail.com>
2017-01-28 19:22:03 +0000

    Friction Prototype
    Added the first prototype for our Friction Based Game.

---
+++ GameBuilds/Prototypes/Friction Prototype/Assets/Scripts/OnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OnClick : MonoBehaviour 
+{
+	public void sceneChanger(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void exitGame()
+	{
+		Application.Quit ();
+	}
+}
---
+++ GameBuilds/Prototypes/Friction Prototype/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float speed = 10;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate ()
+	{
+		if (Input.GetButtonDown ("Player2"))
+		{
+			rb.AddForce (transform.up * speed);
+		}
+	}
+		
+}

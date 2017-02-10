357f38680fd36dece8ec74c319479faf52d7971f
Henry Crofts <hwcrofts@gmail.com>
2017-02-10 08:56:53 +0000

    Friction Prototype
    Removed old version of prototype and uploaded an updated version with
    End Condition script and a basic floor prefab laid out.  Henry.

---
+++ GameBuilds/Prototypes/Friction Prototype/Assets/Scripts/_GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _GameManager : MonoBehaviour
+{
+	#region PlayerController
+	public PlayerController playerController;
+	public PlayerController2 playerController2;
+	#endregion
+
+	void Start ()
+	{
+		
+	}
+
+	void Update () 
+	{
+
+		if(playerController.rb.velocity == 0)
+		{
+			SceneManager.LoadScene(2);
+		}
+
+		if(playerController2.rb.velocity == 0)
+		{
+			SceneManager.LoadScene(3);
+		}
+	}
+}

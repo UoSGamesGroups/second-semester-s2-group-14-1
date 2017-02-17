624f252a8ef56e7dbb05cf10d1b6f2ee7890349d
Michael Curtis <S176514@ucs.ac.uk>
2017-02-17 09:24:28 +0000

    Moved Rafik's build out of the research folder

---
+++ GameBuilds/Rafik's Build/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float MovespeedP1;
+    public float MovespeedP2;
+    // Use this for initialization
+    void Start()
+    {
+        MovespeedP1 = 0.1f;
+        MovespeedP2 = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up") && gameObject.tag == "P1")
+        {
+            transform.Translate(0, MovespeedP1, 0);
+        }
+
+
+
+        if (Input.GetKeyDown("w") && gameObject.tag == "P2")
+        {
+            transform.Translate(0, MovespeedP2, 0);
+        }
+
+    }
+}

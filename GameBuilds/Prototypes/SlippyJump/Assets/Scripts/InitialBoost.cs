1bdb3a69cdfc06ddb7c03ef40e7dc95900dbe5c3
Elliot <elliot@dewhurst.me>
2017-02-23 13:25:49 +0000

    Initial Draft of New Game
    Henry.

---
+++ GameBuilds/Prototypes/SlippyJump/Assets/Scripts/InitialBoost.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialBoost : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public Vector2 initialBoost;
+
+	void Start ()
+    {
+        rb = GetComponent < Rigidbody2D>();
+        rb.velocity = initialBoost;
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
---
+++ GameBuilds/Prototypes/SlippyJump/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float initialBoost;
+
+    public float jump;
+    public float movement;
+    public bool grounded = true;
+
+    public bool shouldStart = false;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = initialBoost;
+        
+	}
+	
+	void Update ()
+    {
+        if (shouldStart)
+        {
+            rb.AddForce(transform.right * initialBoost, ForceMode2D.Impulse);
+            shouldStart = false;
+        }
+        //rb.AddForce(transform.right * movement);
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true)
+        {
+            grounded = false;
+            Jump();
+        }
+	}
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jump);
+    }
+}

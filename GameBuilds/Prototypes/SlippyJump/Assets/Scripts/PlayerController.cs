f08c2eace413f1f85e0a7d2adeb1121497289595
Henry Crofts <hwcrofts@gmail.com>
2017-02-28 13:59:47 +0000

    Made some changes to movement
    Henry.

--- GameBuilds/Prototypes/SlippyJump/Assets/Scripts/PlayerController.cs
+++ GameBuilds/Prototypes/SlippyJump/Assets/Scripts/PlayerController.cs
@@ -1,38 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float initialBoost;
+	public Vector2 initialBoost;
 
     public float jump;
     public float movement;
     public bool grounded = true;
 
-    public bool shouldStart = false;
+	public bool gameEnded = false;
+
+    //public bool shouldStart = false;
 
 	void Start ()
     {
         rb = GetComponent<Rigidbody2D>();
-        //rb.velocity = initialBoost;
+		rb.velocity = initialBoost * Time.deltaTime;
         
 	}
 	
 	void Update ()
     {
-        if (shouldStart)
-        {
-            rb.AddForce(transform.right * initialBoost, ForceMode2D.Impulse);
-            shouldStart = false;
-        }
+        //if (shouldStart)
+        //{
+            //rb.AddForce(transform.right * initialBoost);
+          //  shouldStart = false;
+        //}
         //rb.AddForce(transform.right * movement);
         if (Input.GetKeyDown(KeyCode.W) && grounded == true)
         {
             grounded = false;
             Jump();
         }
+
+		if (rb.velocity.x <= +1) 
+		{
+			gameEnded = true;
+			GameEnded ();
+		}
 	}
 
     void OnCollisionStay2D(Collision2D col)
@@ -47,4 +56,11 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(rb.velocity.x, jump);
     }
+
+	public void GameEnded()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+
 }

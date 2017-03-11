2c52e74b55042847e0b8783ce9f7d5a3af8674db
Henry Crofts <hwcrofts@gmail.com>
2017-03-11 19:07:55 +0000

    Added new visuals to the game
    Henry.

---
+++ GameBuilds/Prototypes/SlippyJump/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+	
+	public AudioClip cheer;
+	public AudioClip brassBand;
+	public AudioClip cannon;
+
+	void Start () 
+	{
+		audio = GetComponent<AudioSource> ();
+		audio.Play (cheer, 1);
+	}
+	
+
+	void Update ()
+	{
+		audio.Play (cheer, 1);
+	}
+}

6d47691b551d7530d4a7f48e4f3bd750be400b58
Lewis <s177026@ucs.ac.uk>
2017-04-28 14:33:13 +0100

    Changes

---
+++ GameBuilds/Prototypes/SlippyJump/Assets/Scripts/CheeseChunks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseChunks : MonoBehaviour 
+{
+	public Rigidbody2D rb;
+
+	public Vector2 chunkMovement;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rb.AddForce(chunkMovement, ForceMode2D.Impulse);
+	}
+
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Ground")
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ GameBuilds/Prototypes/SlippyJump/Assets/Scripts/CheeseWheelController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseWheelController : MonoBehaviour {
+
+	public bool isGrounded;
+	public float waitTime;
+
+	public GameObject[] objectsToThrow;
+
+	// Use this for initialization
+	void Start ()
+	{
+		isGrounded = true;
+		StartCoroutine(throwTimer());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+
+	private IEnumerator throwTimer()
+	{
+		yield return new WaitForSeconds(waitTime);
+		if (isGrounded)
+		{
+			int index = Random.Range(0, objectsToThrow.Length-1);
+			Vector2 thisPos = gameObject.transform.position;
+			Instantiate(objectsToThrow[index], new Vector2(thisPos.x + 0.5f, thisPos.y + 0.5f), Quaternion.identity);
+		}
+		StartCoroutine(throwTimer());
+	}
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+    	if (col.gameObject.tag == "Ground")
+    	{
+    		isGrounded = true;
+    	}
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+    	if (col.gameObject.tag == "Ground")
+    	{
+    		isGrounded = false;
+    	}
+    }
+}

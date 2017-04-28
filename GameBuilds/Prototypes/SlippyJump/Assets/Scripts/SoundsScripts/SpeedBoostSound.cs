b02dd2c8d54f18a6cb94eb7001aa48ac3bc40027
Lewis <s177026@ucs.ac.uk>
2017-04-28 15:02:15 +0100

    Changes 2

---
+++ GameBuilds/Prototypes/SlippyJump/Assets/Scripts/SoundsScripts/SpeedBoostSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoostSound : MonoBehaviour {
+
+	public AudioClip boost;
+	AudioSource audio;
+
+	void Start()
+	{
+		audio = GetComponent<AudioSource>();
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Player1" || col.gameObject.tag == "Player2")
+		{
+			audio.PlayOneShot(boost, 0.5f);
+		}
+	}
+}
